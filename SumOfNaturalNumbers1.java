8002a8ba7ea1c03806c088f83f2e1790dd5b4613
Arif-306 <arifanis306@gmail.com>
2024-10-22 22:49:56 -0700

    Add files via upload

--- SumOfNaturalNumbers1.java
+++ SumOfNaturalNumbers1.java
@@ -1,33 +1,22 @@
-public class MatrixAddition {
-     public static void main(String[] args) {
-         int[][] matrixA = {
-             {1,2,3},
-             {4,5,6},
-             {7,8,9}
-         };
-         int[][] matrixB = {
-             {9,8,7},
-             {6,5,4},
-             {3,2,1}
-         };
-         
-         int[][] result = new int[3][3];
-         
-         result[0][0] = matrixA[0][0] + matrixB[0][0];
-        result[0][1] = matrixA[0][1] + matrixB[0][1];
-        result[0][2] = matrixA[0][2] + matrixB[0][2];
-        
-        result[1][0] = matrixA[1][0] + matrixB[1][0];
-        result[1][1] = matrixA[1][1] + matrixB[1][1];
-        result[1][2] = matrixA[1][2] + matrixB[1][2];
-        
-        result[2][0] = matrixA[2][0] + matrixB[2][0];
-        result[2][1] = matrixA[2][1] + matrixB[2][1];
-        result[2][2] = matrixA[2][2] + matrixB[2][2];
 
-        System.out.println("Resultant Matrix After Addition:");
-        System.out.println(result[0][0] + " " + result[0][1] + " " + result[0][2]);
-        System.out.println(result[1][0] + " " + result[1][1] + " " + result[1][2]);
-        System.out.println(result[2][0] + " " + result[2][1] + " " + result[2][2]);
-     }
+
+
+public class ReverseArray {
+    public static void main(String[] args) {
+       
+        int[] array = {1, 2, 3, 4, 5};
+
+ 
+        System.out.println("Original Array: " + Arrays.toString(array));
+
+       
+        int[] reversedArray = new int[array.length]; 
+        
+        for (int i = 0; i < array.length; i++) {
+            reversedArray[i] = array[array.length - 1 - i]; 
+        }
+
+      
+        System.out.println("Reversed Array: " + Arrays.toString(reversedArray));
+    }
 }
